b81eec6300bda1456896a4b87db44dd4a5ccfbc8
liuyunMo <zhongyy@szfore.com>
2014-05-04 16:50:50 +0800

    说明
    shuoming

--- testShare.lua
+++ testShare.lua
@@ -1,9 +1,10 @@
 function test()
 	local text="just for test from lua!"
 	local imagePath=getImagePath();--
-	share(imagePath,text)
+	share(imagePath,text)--调用分享函数
 end
 
+--分享结束后会调用此函数
 function shareRes( code )
     print(code)
 	if code==0 then
